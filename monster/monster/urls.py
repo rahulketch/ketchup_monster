c4a1b02dbd63467d8804ee114a7f84654d6dec90
Rahul Sharma <rahul.kenu@gmail.com>
2021-04-10 19:09:19 +0200

    Add polls

--- monster/monster/urls.py
+++ monster/monster/urls.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.urls import include, path
 """monster URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,5 +19,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
